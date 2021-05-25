dc1defd11876505e3a90ddc58ca1f3c2161fe7b5
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-05-25 01:32:59 +0100

    Update train.py

--- train.py
+++ train.py
@@ -551,7 +551,7 @@ class Trainer:
         for step in range(self.start_resl, self.max_resl+1):
 
             for iter in tqdm(range(self.iter_start,
-                                   (self.trns_tick+self.stab_tick)*int(ceil(self.datasize/self.batch_size)))):
+                                   (self.trns_tick+self.stab_tick)*int(ceil(800/self.batch_size)))):
 
                 self.iter = iter
                 self.globalIter = self.globalIter+1
