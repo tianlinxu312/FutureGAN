46b60aed4921f6f3db7761ac1290a9efc51414ab
Lin <13871876+tianlinxu312@users.noreply.github.com>
2021-05-25 17:43:10 +0100

    Update mazes.py

--- data/mazes.py
+++ data/mazes.py
@@ -6,6 +6,61 @@ import torch
 from scipy import misc
 
 
+_DATASETS = dict(
+    jaco=DatasetInfo(
+        basepath='jaco',
+        train_size=3600,
+        test_size=400,
+        frame_size=64,
+        sequence_size=11),
+
+    mazes=DatasetInfo(
+        basepath='mazes',
+        train_size=1080,
+        test_size=120,
+        frame_size=84,
+        sequence_size=300),
+
+    rooms_free_camera_with_object_rotations=DatasetInfo(
+        basepath='rooms_free_camera_with_object_rotations',
+        train_size=2034,
+        test_size=226,
+        frame_size=128,
+        sequence_size=10),
+
+    rooms_ring_camera=DatasetInfo(
+        basepath='rooms_ring_camera',
+        train_size=2160,
+        test_size=240,
+        frame_size=64,
+        sequence_size=10),
+
+    rooms_free_camera_no_object_rotations=DatasetInfo(
+        basepath='rooms_free_camera_no_object_rotations',
+        train_size=2160,
+        test_size=240,
+        frame_size=64,
+        sequence_size=10),
+
+    shepard_metzler_5_parts=DatasetInfo(
+        basepath='shepard_metzler_5_parts',
+        train_size=900,
+        test_size=100,
+        frame_size=64,
+        sequence_size=15),
+
+    shepard_metzler_7_parts=DatasetInfo(
+        basepath='shepard_metzler_7_parts',
+        train_size=900,
+        test_size=100,
+        frame_size=64,
+        sequence_size=15)
+)
+_NUM_CHANNELS = 3
+_NUM_RAW_CAMERA_PARAMS = 5
+_MODES = ('train', 'test')
+
+
 class DataReader(object):
     """Minimal queue based TFRecord reader.
       You can use this reader to load the datasets used to train Generative Query
